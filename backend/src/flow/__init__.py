927dddbf4027eb6ccb05292ac3a501e9075f93e1
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-09 21:12:50 +0700

    Revert "test"
    This reverts commit 0fd83094925a3d569a8ae27381093be4560b694e.

--- backend/src/flow/__init__.py
+++ backend/src/flow/__init__.py
@@ -1,6 +1,5 @@
 from flow.base_flow import BaseFlowAnalyze
 # from flow.netflow_analyze import NetflowAnalyze
 from flow.flow_state import FlowState, FlowActionState
-from flow.flow_table_watch_task import FlowTableWatchTask
 
-__all__ = ["BaseFlowAnalyze", "FlowState", "FlowActionState", "FlowTableWatchTask"]
+__all__ = ["BaseFlowAnalyze", "FlowState", "FlowActionState"]
