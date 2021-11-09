571faa842949821d6cb621c5310503df567b8cca
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-09 14:09:41 +0700

    test

--- api(20-3-2562)/api_change_route.py
+++ api(20-3-2562)/api_change_route.py
@@ -58,12 +58,12 @@ def change_route(path, src_mgmtip, dst_mgmtip):
 def get_path(src_mgmtip, dst_mgmtip):
     paths = requests.get("http://"+ip+":5001/api/v1/path/"+src_mgmtip+","+dst_mgmtip).json()
     nexthop_node = find_nexthop_node(src_mgmtip, dst_mgmtip)
-    change_route(['192.168.8.1', '192.168.7.34', '192.168.7.17', '192.168.1.1', '192.168.1.2', '192.168.4.2'], src_mgmtip, dst_mgmtip)
+    change_route(['192.168.8.1', '192.168.7.34', '192.168.7.17', '192.168.1.1', '192.168.1.2'], src_mgmtip, dst_mgmtip)
 
 
 #get_mask("100.3.11.1")
 #find_nexthop_node("100.3.11.1", "100.3.12.1")
-get_path("192.168.8.1", "192.168.4.2") # A->A'
+get_path("192.168.8.1", "192.168.1.2") # A->A'
 #get_path("100.1.3.1", "100.1.5.1") # A'->A''
 #get_path("100.1.5.1", "100.1.1.1") # A''->A
 #get_path("100.1.2.1", "100.1.4.1") # B->B'
