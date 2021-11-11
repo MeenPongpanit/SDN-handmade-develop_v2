ef7ead2639197d67c8300f5ec0a0687dfd7b48b9
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-11 17:00:05 +0700

    Update sdn_utils.py

--- backend/src/sdn_utils.py
+++ backend/src/sdn_utils.py
@@ -109,71 +109,71 @@ def generate_flow_remove_command(flow):
     return [remove_acl, remove_route_map]
 
 
-def generate_flow_command(flow, action, current_action):
-    acl_command1 = "ip access-list extended SDN-{}".format(flow['name'])
-    acl_command0 = "no " + acl_command1
-    acl_command2 = "remark Generate by SDN Handmade for flow name {}".format(flow['name'])
-    # For debugging
-    acl_command3 = "10 permit udp"
-    if flow['pending']['src_ip'] == 'any':
-        acl_command3 += ' any'
-    elif flow['pending']['src_wildcard'] is None:
-        acl_command3 += ' host {}'.format(flow['pending']['src_ip'])
-    else:
-        acl_command3 += ' {} {}'.format(flow['pending']['src_ip'], flow['pending']['src_wildcard'])
-    if flow['pending']['src_port'] is not None:
-        if '-' in flow['pending']['src_port']:
-            port = flow['pending']['src_port'].split('-')
-            start_port = port[0]
-            end_port = port[1]
-            acl_command3 += ' range {} {}'.format(start_port, end_port)
-        else:
-            acl_command3 += ' eq {}'.format(flow['pending']['src_port'])
+# def generate_flow_command(flow, action, current_action):
+#     acl_command1 = "ip access-list extended SDN-{}".format(flow['name'])
+#     acl_command0 = "no " + acl_command1
+#     acl_command2 = "remark Generate by SDN Handmade for flow name {}".format(flow['name'])
+#     # For debugging
+#     acl_command3 = "10 permit udp"
+#     if flow['pending']['src_ip'] == 'any':
+#         acl_command3 += ' any'
+#     elif flow['pending']['src_wildcard'] is None:
+#         acl_command3 += ' host {}'.format(flow['pending']['src_ip'])
+#     else:
+#         acl_command3 += ' {} {}'.format(flow['pending']['src_ip'], flow['pending']['src_wildcard'])
+#     if flow['pending']['src_port'] is not None:
+#         if '-' in flow['pending']['src_port']:
+#             port = flow['pending']['src_port'].split('-')
+#             start_port = port[0]
+#             end_port = port[1]
+#             acl_command3 += ' range {} {}'.format(start_port, end_port)
+#         else:
+#             acl_command3 += ' eq {}'.format(flow['pending']['src_port'])
 
-    if flow['pending']['dst_ip'] == 'any':
-        acl_command3 += ' any'
-    elif flow['pending']['dst_wildcard'] is None:
-        acl_command3 += ' host {}'.format(flow['pending']['dst_ip'])
-    else:
-        acl_command3 += ' {} {}'.format(flow['pending']['dst_ip'], flow['pending']['dst_wildcard'])
-    if flow['pending']['dst_port'] is not None:
-        if '-' in flow['pending']['dst_port']:
-            port = flow['pending']['dst_port'].split('-')
-            start_port = port[0]
-            end_port = port[1]
-            acl_command3 += ' range {} {}'.format(start_port, end_port)
-        else:
-            acl_command3 += ' eq {}'.format(flow['pending']['dst_port'])
+#     if flow['pending']['dst_ip'] == 'any':
+#         acl_command3 += ' any'
+#     elif flow['pending']['dst_wildcard'] is None:
+#         acl_command3 += ' host {}'.format(flow['pending']['dst_ip'])
+#     else:
+#         acl_command3 += ' {} {}'.format(flow['pending']['dst_ip'], flow['pending']['dst_wildcard'])
+#     if flow['pending']['dst_port'] is not None:
+#         if '-' in flow['pending']['dst_port']:
+#             port = flow['pending']['dst_port'].split('-')
+#             start_port = port[0]
+#             end_port = port[1]
+#             acl_command3 += ' range {} {}'.format(start_port, end_port)
+#         else:
+#             acl_command3 += ' eq {}'.format(flow['pending']['dst_port'])
 
-    acl_command4 = acl_command3.replace('10 permit udp', '20 permit tcp')
-    acl_command5 = acl_command3.replace('10 permit udp', '30 permit icmp')
+#     acl_command4 = acl_command3.replace('10 permit udp', '20 permit tcp')
+#     acl_command5 = acl_command3.replace('10 permit udp', '30 permit icmp')
 
-    if action is None:
-        return [acl_command0, acl_command1, acl_command2, acl_command3, acl_command4, acl_command5]
+#     if action is None:
+#         return [acl_command0, acl_command1, acl_command2, acl_command3, acl_command4, acl_command5]
 
-    map_seq = flow.get('seq')
+#     map_seq = flow.get('seq')
 
-    # Route Map
-    rmap_1 = "route-map SDN-topo permit {}".format(map_seq)
-    rmap_2 = "match ip address SDN-{}".format(flow['name'])
+#     # Route Map
+#     rmap_1 = "route-map SDN-topo permit {}".format(map_seq)
+#     rmap_2 = "match ip address SDN-{}".format(flow['name'])
 
-    rmap_21 = ''
-    if current_action is not None:
-        if current_action['rule'] == 'drop':
-            rmap_21 = 'no set interface Null0'
-        elif current_action['rule'] == 'next-hop':
-            rmap_21 = 'no set ip next-hop {}'.format(current_action['data'])
-        elif current_action['rule'] == 'exit-if':
-            rmap_21 = 'no set interface {}'.format(current_action['data'])
+#     rmap_21 = ''
+#     if current_action is not None:
+#         if current_action['rule'] == 'drop':
+#             rmap_21 = 'no set interface Null0'
+#         elif current_action['rule'] == 'next-hop':
+#             rmap_21 = 'no set ip next-hop {}'.format(current_action['data'])
+#         elif current_action['rule'] == 'exit-if':
+#             rmap_21 = 'no set interface {}'.format(current_action['data'])
 
-    if action['rule'] == 'drop':
-        rmap_3 = 'set interface Null0'
-    elif action['rule'] == 'next-hop':
-        rmap_3 = 'set ip next-hop {}'.format(action['data'])
-    elif action['rule'] == 'exit-if':
-        rmap_3 = 'set interface {}'.format(action['data'])
-    else:
-        rmap_3 = ''
+#     if action['rule'] == 'drop':
+#         rmap_3 = 'set interface Null0'
+#     elif action['rule'] == 'next-hop':
+#         rmap_3 = 'set ip next-hop {}'.format(action['data'])
+#     elif action['rule'] == 'exit-if':
+#         rmap_3 = 'set interface {}'.format(action['data'])
+#     else:
+#         rmap_3 = ''
 
-    return [acl_command0, acl_command1, acl_command2, acl_command3, acl_command4, acl_command5, rmap_1, rmap_2, rmap_21,
-            rmap_3]
+#     return [acl_command0, acl_command1, acl_command2, acl_command3, acl_command4, acl_command5, rmap_1, rmap_2, rmap_21,
+#             rmap_3]
