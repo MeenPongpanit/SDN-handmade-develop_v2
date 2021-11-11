3083590505e6576d56382d675f58d5688fab6619
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-11 15:39:56 +0700

    Update graph.py

--- backend/src/api/v1/graph.py
+++ backend/src/api/v1/graph.py
@@ -5,9 +5,25 @@ from sanic.views import HTTPMethodView
 
 class GraphView(HTTPMethodView):
 
+    # def get(self, request):
+    #     data = request.app.db['link_utilization'].get_all()['links']
+    #     nodes = {}
+    #     edges = {}
+    #     for link in data:
+    #         src_node = link['src_node_hostname']
+    #         dst_node = link['dst_node_hostname']
+    #         if src_node not in nodes:
+    #             nodes[src_node] = f'node{len(nodes)}'
+    #         if dst_node not in nodes:
+    #             nodes[dst_node] = f'node{len(nodes)}'
+    #         edges[f'edge{len(edges)}'] = {'source':nodes[src_node], 'target':nodes[dst_node]}
+    #     nodes = {nodes[i]:{'name':i} for i in nodes}
+    #     graph = {"nodes":nodes, "edges":edges}
+    #     flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
+    #     return json({"graph": graph, "status": "ok"}, dumps=dumps)
+
     def get(self, request):
         data = request.app.db['link_utilization'].get_all()
-        print(data)
         nodes = {}
         edges = {}
         for link in data:
@@ -21,6 +37,6 @@ class GraphView(HTTPMethodView):
         nodes = {nodes[i]:{'name':i} for i in nodes}
         graph = {"nodes":nodes, "edges":edges}
         flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
-        return json({"graph": graph, "status": "ok"}, dumps=dumps)
+        return json(data, dumps=dumps)
 
 
