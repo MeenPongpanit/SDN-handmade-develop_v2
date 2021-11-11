d26e8cc2b2724042a2c3501a167b5b426ceba619
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-11 15:41:01 +0700

    Update graph.py

--- backend/src/api/v1/graph.py
+++ backend/src/api/v1/graph.py
@@ -5,25 +5,15 @@ from sanic.views import HTTPMethodView
 
 class GraphView(HTTPMethodView):
 
-    # def get(self, request):
-    #     data = request.app.db['link_utilization'].get_all()['links']
-    #     nodes = {}
-    #     edges = {}
-    #     for link in data:
-    #         src_node = link['src_node_hostname']
-    #         dst_node = link['dst_node_hostname']
-    #         if src_node not in nodes:
-    #             nodes[src_node] = f'node{len(nodes)}'
-    #         if dst_node not in nodes:
-    #             nodes[dst_node] = f'node{len(nodes)}'
-    #         edges[f'edge{len(edges)}'] = {'source':nodes[src_node], 'target':nodes[dst_node]}
-    #     nodes = {nodes[i]:{'name':i} for i in nodes}
-    #     graph = {"nodes":nodes, "edges":edges}
-    #     flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
-    #     return json({"graph": graph, "status": "ok"}, dumps=dumps)
-
     def get(self, request):
         data = request.app.db['link_utilization'].get_all()
+        print("================")
+        print("================")
+        print("================")
+        print(data)
+        print("================")
+        print("================")
+        print("================")
         nodes = {}
         edges = {}
         for link in data:
@@ -37,6 +27,6 @@ class GraphView(HTTPMethodView):
         nodes = {nodes[i]:{'name':i} for i in nodes}
         graph = {"nodes":nodes, "edges":edges}
         flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
-        return json(data, dumps=dumps)
+        return json({"graph": graph, "status": "ok"}, dumps=dumps)
 
 
