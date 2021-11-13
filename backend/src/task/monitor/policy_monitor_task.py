b2ef38f2b0e124340c1e8d206a1312b0c2d52a0b
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-13 21:45:37 +0700

    Update policy_monitor_task.py

--- backend/src/task/monitor/policy_monitor_task.py
+++ backend/src/task/monitor/policy_monitor_task.py
@@ -196,11 +196,11 @@ class PolicyMonitorTask:
             cmd = generate_config_command(device['type'], new_flow, flow_id, flow_name, action)
             # Policy cmd + action cmd
             # device_list[action['management_ip']] = ["\n".join(policy_cmd + action_cmd)]
-            print("@@@@@@@@@@@@@@@@@@@@@@")
-            print("@@@@@@@@@@@@@@@@@@@@@@")
+            print("############################")
+            print("############################")
             print(cmd)
-            print("@@@@@@@@@@@@@@@@@@@@@@")
-            print("@@@@@@@@@@@@@@@@@@@@@@")
+            print("############################")
+            print("############################")
             device_list[device["management_ip"]] = cmd
 
         # Remove old action
