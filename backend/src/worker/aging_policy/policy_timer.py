8ea6cba527be98cbb3eddb974104b0dd10c6d201
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-20 17:12:26 +0700

    t

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -1,20 +1,20 @@
 import time
 from pymongo import MongoClient
+import requests
 from threading import Thread 
 import time
 
 class MyThread(Thread):
-    def __init__(self, timeout):
+    def __init__(self, flow_id, timeout):
         Thread.__init__(self)
+        self.flow_id = str(flow_id)
         self.timeout = timeout
+        self.controller_ip = '10.50.34.37'
 
     def run(self):
         time.sleep(self.timeout)
-        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-        print("NOW DELETE THIS")
-        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        payload = {'flow_id': self.flow_id}
+        response = requests.delete("http://"+self.controller_ip+":5001/api/v1/flow/routing",  params=payload)
 
 class TimerPolicyWorker:
     def __init__(self, policy_number):
@@ -26,10 +26,10 @@ class TimerPolicyWorker:
     def run(self):
         while True:
             for obj in self.flow:
-                if obj['_id'] not in self.running_policy:
-                    self.running_policy.append(obj['_id'])
+                if obj['flow_id'] not in self.running_policy:
+                    self.running_policy.append(obj['flow_id'])
                     timeout = 10
-                    tread_obj = MyThread(timeout)
+                    tread_obj = MyThread(obj['flow_id'], timeout)
                     tread_obj.start()
             print("############################")
             print(self.running_policy)
