cf9d31292b92d2137baf908245c3fd4eba5fae7c
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-20 14:44:20 +0700

    Update policy_timer.py

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -5,14 +5,18 @@ class TimerPolicyWorker:
     def __init__(self, policy_number):
         self.policy_number = policy_number
         self.client = MongoClient('localhost', 27017)
-        self.flow = self.client.sdn01.flow_routing.find() 
+        self.flow = self.client.sdn01.flow_routing.find()
+        self.running_policy = []
+
+    def update_currentflow(self):
+        for obj in self.flow:
+            self.running_policy.append(obj)
+            print("############################")
+            print(self.running_policy)
+            print("############################")
 
     def run(self):
-        running_policy = []
         while True:
-            for obj in self.flow:
-                running_policy.append(obj)
-            print("############################")
-            print(running_policy)
-            print("############################")
+            self.update_currentflow(self)
             time.sleep(1)
+    
