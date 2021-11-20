f99c42dcb7dd104474ec33f5df7469ed9b4216e1
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-20 14:45:16 +0700

    Update policy_timer.py

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -8,15 +8,11 @@ class TimerPolicyWorker:
         self.flow = self.client.sdn01.flow_routing.find()
         self.running_policy = []
 
-    def update_currentflow(self):
-        for obj in self.flow:
-            self.running_policy.append(obj)
+    def run(self):
+        
+        while True:
             print("############################")
             print(self.running_policy)
             print("############################")
-
-    def run(self):
-        while True:
-            self.update_currentflow(self)
             time.sleep(1)
     
