ac32470cc99ec12f2cfc94ec18269de54792840b
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-20 18:15:50 +0700

    update flow timer

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -4,9 +4,6 @@ import requests
 import time
 import concurrent.futures
 
-
-    
-
 class TimerPolicyWorker:
     def __init__(self, policy_number):
         self.policy_number = policy_number
@@ -21,28 +18,23 @@ class TimerPolicyWorker:
             time.sleep(timeout)
             #if condition last switch too long
             payload = {'flow_id': flow_id}
-            print("@@@@@@@@@@@@@@")
-            print("Policy %d NOW REMOVE" %(flow_id))
-            print("@@@@@@@@@@@@@@")
-            response = requests.delete("http://localhost:5001/api/v1/flow/routing",  params=payload)
+            requests.delete("http://localhost:5001/api/v1/flow/routing",  params=payload)
             return flow_id
         
         while True:
             self.flow = self.client.sdn01.flow_routing.find()
             for obj in self.flow:
-                print(obj)
                 try:
                     if obj['flow_id'] not in self.running_policy:
                         self.running_policy.append(obj['flow_id'])
         
                         with concurrent.futures.ThreadPoolExecutor() as executor:
                             future = executor.submit(delete, obj['flow_id'], self.timeout)
-                            return_value = future.result()
-                            print("########################")
-                            print(return_value)
-                            self.running_policy.remove(return_value)
+                            flow_id = future.result()
+                            print("Flow is Remove due to timeout")
+                            self.running_policy.remove(flow_id)
                 except:
                     print("updating flow in process")
             print(self.running_policy)
-            time.sleep(1)
+            time.sleep(2)
     
