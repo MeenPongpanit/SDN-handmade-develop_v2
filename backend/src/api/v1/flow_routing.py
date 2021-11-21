a44f2c05e353397ee6b282738669c8bebf469827
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-21 20:33:55 +0700

    Update flow_routing.py

--- backend/src/api/v1/flow_routing.py
+++ backend/src/api/v1/flow_routing.py
@@ -57,8 +57,7 @@ class FlowRoutingView(HTTPMethodView):
         policy_repo.add_or_update_flow_routing(policy)
 
         obj_id = policy['new_flow']['src_ip'] + "-" + policy['new_flow']['src_port'] + "-" + policy['new_flow']['dst_ip'] + "-" + policy['new_flow']['src_port']
-        # t1 = TimerPolicyWorker(obj_id)
-        # t1.run()
+        TimerPolicyWorker(obj_id).run()
         
         print("=================")
         print("=================")
