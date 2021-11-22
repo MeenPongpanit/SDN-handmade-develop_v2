ec1ca4ccb1eebcf8bc785ca490a05c8f7848cd19
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-22 16:26:45 +0700

    Update policy_timer.py

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -13,23 +13,14 @@ class Counter(Thread):
 
     def run(self):
         time.sleep(self.timeout)
-        key = self.key.split('-')
+        key = self.key
         flow = MongoClient('localhost', 27017).sdn01.flow_routing.find()
 
         for obj in flow:
             if key[0] == obj['src_ip'] and key[1] == obj['src_port'] and key[2] == obj['dst_ip'] and key[3] == obj['dst_port']:
-                print("@@@@@@@@@@@@@")
-                print("@@@@@@@@@@@@@")
-                print("@@@@@@@@@@@@@")
-                print(obj['flow_id'])
-                print("@@@@@@@@@@@@@")
-                print("@@@@@@@@@@@@@")
-                print("@@@@@@@@@@@@@")
-        # payload = {'flow_id':'1'}
-        # requests.delete("http://localhost:5001/api/v1/flow/routing",  params=payload)
-        # print("@@@@@@@@@@@@@@")
-        # print("@@@@@@@@@@@@@@")
-        # print("@@@@@@@@@@@@@@")
+                payload = {'flow_id':str(obj['flow_id'])}
+                requests.delete("http://localhost:5001/api/v1/flow/routing",  params=payload)
+                break
 
 
 class TimerPolicyWorker:
@@ -37,7 +28,7 @@ class TimerPolicyWorker:
         self.obj_id = obj_id
         self.client = MongoClient('localhost', 27017)   
         self.timeout = 20
-        self.running_policy = []
+
 
     def run(self):
 
@@ -45,15 +36,7 @@ class TimerPolicyWorker:
             self.flow = self.client.sdn01.flow_routing.find()
             for obj in self.flow:
                 if len(obj) == 14:
-                    key = obj['src_ip'] + "-" + obj['src_port'] + "-" + obj['dst_ip'] + "-" + obj['dst_port']
-                    if key not in self.running_policy:
-                        self.running_policy.append(key)    
-                        Counter(key).start()
+                    key = (obj['src_ip'] + "-" + obj['src_port'] + "-" + obj['dst_ip'] + "-" + obj['dst_port']).split("-")
+                    Counter(key).start()
 
-
-            print(self.running_policy)
-            print("############################")
-
-            # print(self.running_policy)
-            print("############################")
             time.sleep(2)
