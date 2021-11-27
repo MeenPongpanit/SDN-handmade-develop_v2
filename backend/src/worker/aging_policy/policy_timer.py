2529e8eb29bb97e7a18e881bdced04f290a5931b
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-27 13:47:36 +0700

    Update policy_timer.py

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -4,14 +4,14 @@ import requests
 import time
 import concurrent.futures
 from threading import Thread
-from ipaddress import IPv4Network, IPv4Address
+from ipaddress import IPv4Network, IPv4Address, ip_network
 
 
 class Counter(Thread):
     def __init__(self, key, client):
         Thread.__init__(self)
         self.key = key
-        self.timeout = 10
+        self.timeout = 5
         self.client = client
 
     def run(self):
@@ -25,31 +25,26 @@ class Counter(Thread):
         time.sleep(self.timeout)
         while True:
             
-            src_ip_list, dst_ip_list = [], []
-            src_prefix, dst_prefix = IPv4Address._prefix_from_ip_int(int(IPv4Address(self.key['src_wildcard']))^(2**32-1)), IPv4Address._prefix_from_ip_int(int(IPv4Address(self.key['dst_wildcard']))^(2**32-1))
-            src_network_obj = IPv4Network(convert_ip_to_network(self.key['src_ip'], int(src_prefix)) + '/' + str(src_prefix))
-            dst_network_obj = IPv4Network(convert_ip_to_network(self.key['dst_ip'], int(dst_prefix)) + '/' + str(dst_prefix))
+            # src_ip_list, dst_ip_list = [], []
+            # src_prefix, dst_prefix = IPv4Address._prefix_from_ip_int(int(IPv4Address(self.key['src_wildcard']))^(2**32-1)), IPv4Address._prefix_from_ip_int(int(IPv4Address(self.key['dst_wildcard']))^(2**32-1))
+            # src_network_obj = IPv4Network(convert_ip_to_network(self.key['src_ip'], int(src_prefix)) + '/' + str(src_prefix))
+            # dst_network_obj = IPv4Network(convert_ip_to_network(self.key['dst_ip'], int(dst_prefix)) + '/' + str(dst_prefix))
             
-            for i in src_network_obj:
-                src_ip_list.append(str(i))
-            for i in dst_network_obj:
-                dst_ip_list.append(str(i))
+            query_filter = {}
+            print("=======")
+            print(self.key)
+            print("=======")
+            for i in self.key:
+                if self.key[i].lower() != 'any':
+                    if 'addr' in i:
+                        ip_prefix = IPv4Address._prefix_from_ip_int(int(IPv4Address(self.key[i]))^(2**32-1))
+                        ip_network = IPv4Network(convert_ip_to_network(self.key[i], int(ip_prefix)) + '/' + str(ip_prefix))
+                        query_filter[i] = {'$in':[str(i) for i in ip_network]}
+                    else:
+                        query_filter[i] = int(self.key[i])
 
-            if str(self.key['src_port']).lower() == 'any' and str(self.key['dst_port']).lower() == 'any':
-                flows = self.client.sdn01.flow_stat.find({ 'ipv4_src_addr': {'$in': src_ip_list} ,  'ipv4_dst_addr': {'$in': dst_ip_list} } )
-                print("444444444444444444444444444444444444")
-                print("444444444444444444444444444444444444")
-            elif str(self.key['src_port']).lower() == 'any':
-                flows = self.client.sdn01.flow_stat.find({ 'ipv4_src_addr': {'$in': src_ip_list} ,  'ipv4_dst_addr': {'$in': dst_ip_list}, 'l4_dst_port': {'$in': [int(self.key['dst_port'])]} } )
-
-
-            elif str(self.key['dst_port']).lower() == 'any':
-                flows = self.client.sdn01.flow_stat.find({ 'ipv4_src_addr': {'$in': src_ip_list} ,  'ipv4_dst_addr': {'$in': dst_ip_list}, 'l4_src_port': {'$in': [int(self.key['src_port'])]} } )
-
-                # print(src_ip_list)
-
-            else:
-                flows = self.client.sdn01.flow_stat.find({ 'ipv4_src_addr': {'$in': src_ip_list} ,  'ipv4_dst_addr': {'$in': dst_ip_list}, 'l4_src_port': {'$in': [int(self.key['src_port'])] }, 'l4_dst_port': {'$in': [int(self.key['dst_port'])]} } )
+            flows = self.client.sdn01.flow_stat.find(query_filter)
+           
 
             check = []
             for i in flows:
@@ -59,12 +54,9 @@ class Counter(Thread):
                 time.sleep(self.timeout)
             else:
                 print(len(check))
-                print("#########################")
-                print("#########################")
-                print("#########################")
                 payload = {'flow_id': self.key['flow_id']}
                 print(payload)
-                requests.delete("http://localhost:5001/api/v1/flow/routing",  params=payload)
+                # requests.delete("http://localhost:5001/api/v1/flow/routing",  params=payload)
                 break
 
 
@@ -80,4 +72,4 @@ class TimerPolicyWorker:
                 if len(obj) == 14:
                     key = {i:obj[i] for i in ['src_ip', 'src_port', 'dst_ip', 'dst_port', 'src_wildcard', 'dst_wildcard', 'flow_id']}
                     Counter(key, self.client).start()
-            time.sleep(10)
+            time.sleep(60)
