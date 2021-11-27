3c0b801b9a35b3cbc84f773ab52fc791e2fe7226
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-11-27 14:23:44 +0700

    Update policy_timer.py

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -42,7 +42,8 @@ class Counter(Thread):
                     if 'addr' in i:
                         index = i + '_wildcard'
                         print(index)
-                        ip_prefix = IPv4Address._prefix_from_ip_int(int(IPv4Address(self.info[i]))^(2**32-1))
+                        ip_prefix = IPv4Address._prefix_from_ip_int(int(IPv4Address(self.info[index]))^(2**32-1))
+                        print(ip_prefix)
                         ip_network = IPv4Network(convert_ip_to_network(self.key[i], int(ip_prefix)) + '/' + str(ip_prefix))
                         print(ip_network)
                         query_filter[i] = {'$in':[str(i) for i in ip_network]}
