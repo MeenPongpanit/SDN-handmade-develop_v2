8d8041df79480d5694f9b1189575ee0192602184
MeenPongpanit <61070124@kmitl.ac.th>
2021-12-15 14:52:30 +0700

    Update graph.py

--- backend/src/api/v1/graph.py
+++ backend/src/api/v1/graph.py
@@ -32,11 +32,21 @@ class GraphView(HTTPMethodView):
         data = loads(dumps(request.app.db['link_utilization'].get_all()))
         nodes = {}
         edges = {}
-        flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
+        flows_by_edge = {}
 
+        flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
+        flows_data = []
         filtered_flow = []
         for flow in flows:
-
+            flow_data = {
+                'src_ip':flow['ipv4_src_addr'], 
+                'dst_ip':flow['ipv4_dst_addr'], 
+                'src_port':flow['l4_src_port'],
+                'dst_port':flow['l4_dst_port'],
+                'next_hop_ip':flow['ipv4_next_hop'],
+                'flow_rate':flow.get('Mbits_per_sec', '')
+                }
+            flows_data.append(flow_data)
             if flow['l4_dst_port'] in filters or flow['l4_src_port'] in filters :
                 filtered_flow.append(flow['ipv4_next_hop'])
 
@@ -47,12 +57,17 @@ class GraphView(HTTPMethodView):
                 nodes[src_node] = f'node{len(nodes)}'
             if dst_node not in nodes:
                 nodes[dst_node] = f'node{len(nodes)}'
-            edge_id = len(edges)
-            edges[f'edge{edge_id}'] = {'source':nodes[src_node], 'target':nodes[dst_node]}
+            edge_id = f'edge{len(edges)}'
+            edges[edge_id] = {'source':nodes[src_node], 'target':nodes[dst_node]}
+            flows_by_edge[edge_id] = []
             if link['dst_if_ip'] in filtered_flow or link['src_if_ip'] in filtered_flow or not filters:
                 edges[f'edge{edge_id}']['animate'] = True
+            for flow_data in flows_data:
+                if flow_data['next_hop_ip'] in (link['dst_if_ip'], link['src_if_ip']):
+                    flows_by_edge[edge_id].append(flow_data)
+
         nodes = {nodes[i]:{'name':i} for i in nodes}
-        graph = {"nodes":nodes, "edges":edges}
+        graph = {"nodes":nodes, "edges":edges, "flows":flows_by_edge}
         print("#####################")
         print(graph['edges'])
         print("#####################")
