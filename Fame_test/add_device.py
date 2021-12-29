22d67ab2a26161d0e9f1e0e7872fbea7fa7b5483
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-12-29 15:22:38 +0700

    t

--- Fame_test/add_device.py
+++ Fame_test/add_device.py
@@ -5,9 +5,7 @@ import requests
 
 
 controller_ip = '10.50.34.15'
-
 device_list = ['192.168.1.1', '192.168.1.2', '192.168.7.17', '192.168.7.18', '192.168.7.49', '192.168.3.1', '192.168.4.2']
-
 def add_device():
     for device in device_list:
         print("adding device", device)
@@ -37,20 +35,16 @@ def remove_device(id):
     print("send command remove device id:", id)
     requests.delete("http://" + controller_ip + ":5001/api/v1/device",  params={'device_id': id})
 
-def init():
-    print("Do Initialize")
+def initialize():
     requests.post("http://" + controller_ip +  ":5001/api/v1/initialization", json={'service': 'snmp', 'management_ip':controller_ip})
-    print("Init Finish")
 
-def net_flow():
-    print("Do NetFlow")
+def set_netflow():
     requests.post("http://" + controller_ip +  ":5001/api/v1/initialization", json={'service': 'netflow', 'management_ip':controller_ip})
-    print("NetFlow Done")
 
 def do_all():
     add_device()
-    init()
-    net_flow()
+    initialize()
+    set_netflow()
 
 def policy_test():
     src_net = '192.168.8.0'
@@ -59,14 +53,12 @@ def policy_test():
     dst_net = '192.168.10.0'
     dst_port = '5555'
     dst_wildcard = '0.0.0.255'
-    action = [{'device_id':'61bad78114f944ac9721a8fc', 'action':2, 'data':'192.168.7.49'}]
-    new_flow = {'name':'new_route', 'src_ip':src_net, 'src_port':src_port, 'src_subnet':src_wildcard, 'dst_ip':dst_net, 'dst_port':dst_port, 'dst_subnet':dst_wildcard, 'actions':action}
+    action = [{'device_id':'61baf52c14f944ac9726c288', 'action':2, 'data':'192.168.7.49'}]
+    new_flow = {'name':'new_route', 'src_ip':src_net, 'src_port':src_port, 'src_subnet':src_wildcard, 'dst_ip':dst_net, 'dst_port':dst_port,\
+         'dst_subnet':dst_wildcard, 'actions':action, 'aging_time': 60}
     requests.post("http://"+controller_ip+":5001/api/v1/flow/routing", json=new_flow)
-    action = [{'device_id':'61bad78914f944ac9721a9ae', 'action':2, 'data':'192.168.7.34'}]
-    new_flow = {'name':'new_route', 'src_ip':'192.168.10.0', 'src_port':'5555', 'src_subnet':'0.0.0.255', 'dst_ip':'192.168.8.0', 'dst_port':'any', 'dst_subnet':'0.0.0.255', 'actions':action}
-    requests.post("http://"+controller_ip+":5001/api/v1/flow/routing", json=new_flow)
-    print("Done")
-
+    action = [{'device_id':'61baf53414f944ac9726c332', 'action':2, 'data':'192.168.7.34'}]
+    
 def main():
     print("1 : Add Device")
     print("2 : Remove All Device ")
@@ -80,14 +72,12 @@ def main():
     elif action == '2':
         remove_all_device()
     elif action == '3':
-        init()
+        initialize()
     elif action == '4':
-        net_flow()
+        set_netflow()
     elif action == '5':
         do_all()
     else:
         policy_test()
 
-
 main()
-
